7fc9ee8cb2de05bb20ab225f8fbaf5e63e6ce425
Raghu Varma <raghuvarma331@gmail.com>
2019-08-14 00:12:43 +0530

    Update build.whyred.sh

--- build.whyred.sh
+++ build.whyred.sh
@@ -34,6 +34,20 @@ LINEAGE-SOURCE()
     git clone https://github.com/RaghuVarma331/vendor_MiuiCamera.git -b pie vendor/MiuiCamera
 } 
 
+ONUI_PENDRO-SOURCE()
+{
+    mkdir onui
+    cd onui
+    echo -ne '\n' | repo init -u https://github.com/onui-pendro-1-5/onui_pendro_manifest.git -b pie --depth=1
+    repo sync -c --no-tags --no-clone-bundle -f --force-sync -j16
+    rm -r packages/resources/devicesettings   
+    git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-16.0 packages/resources/devicesettings
+    git clone https://github.com/RaghuVarma331/android_kernel_xiaomi_whyred.git --depth=1 -b pie-whyred kernel/xiaomi/whyred
+    git clone https://github.com/RaghuVarma331/android_device_xiaomi_whyred.git -b onui-pie device/xiaomi/whyred
+    git clone https://github.com/RaghuVarma331/vendor_xiaomi_whyred.git -b pie vendor/xiaomi/whyred
+    git clone https://github.com/RaghuVarma331/vendor_MiuiCamera.git -b pie vendor/MiuiCamera
+} 
+
 PE-SOURCE()
 {
     mkdir pe
@@ -105,13 +119,26 @@ BUILD_pixelexperiance()
 	rm -r pe		
 }
 
+BUILD_onuipendro()
+{	
+        . build/envsetup.sh && lunch aosp_whyred-userdebug && make -j32 bacon
+ 	cd out/target/product/whyred
+        rm -r android-info.txt  dex_bootjars obj_arm  product_copy_files_ignored.txt  symbols boot.img fake_packages ramdisk.img system build_fingerprint.txt gen ramdisk-recovery.img userdata.img build_thumbprint.txt install recovery clean_steps.mk kernel recovery.id data obj previous_build_config.mk  root vendor
+        cd ..
+	cp -r whyred /var/lib/jenkins/workspace/Xiaomi
+	cd 
+	cd /var/lib/jenkins/workspace/Xiaomi
+	mv whyred pendro
+	rm -r onui
+}
+
 # Main Menu
 clear
 echo "----------------------------------------------------------------------------------------"
 echo "Welcome To Whyred Redmi note5 pro Remote Script Made By Raghu Varma"
 echo "Coded By Raghu Varma.G #Developer"
 echo "----------------------------------------------------------------------------------------"
-PS3='Please select your option (1-4): '
+PS3='Please select your option (1-5): '
 menuvar=("BasicSetup" "LineageOS"  "PixelExperience" "all_roms" "Exit")
 select menuvar in "${menuvar[@]}"
 do
@@ -175,6 +202,43 @@ do
             read -n1 -r key
             break
             ;;
+        "OnUI-Pendro")
+            clear
+            echo "----------------------------------------------"
+            echo "Started Building OnUI-Pendro For whyred  ."
+            echo "Please be patient..."
+            # OnUI-Pendro
+            echo "----------------------------------------------"
+            echo "Setting Up Tools & Stuff..."
+            echo " "
+            TOOLS_SETUP
+	    echo " "	    
+            echo "----------------------------------------------"
+            echo "Setting up Environment & source..."
+            echo " "
+            ONUI_PENDRO-SOURCE
+	    echo " "
+            echo "----------------------------------------------"
+            echo "Cleaning up source..."
+            echo " "
+            CLEAN_SOURCE            
+	    echo " "
+            echo "----------------------------------------------"
+            echo "Compilation Started..."
+            echo " "
+            BUILD_onuipendro
+            echo " "
+            echo "----------------------------------------------"
+            echo "OnUI-Pendro Rom build finished."
+            echo " "
+            echo "----------------------------------------------"
+            echo "whyred 2019 OnUI-Pendro build finished."
+            echo "Flashable zip is located into /root."
+            echo "Press any key for end the script."
+            echo "----------------------------------------------"
+            read -n1 -r key
+            break
+            ;;	   	    
         "PixelExperience")
             clear
             echo "----------------------------------------------"
